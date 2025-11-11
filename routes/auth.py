947275f362be10b91f7d82ae23a4941c03d14f8d
Alweezy <alvinmutisya@gmail.com>
2025-11-11 17:10:19 +0300

    Redirect user on successful auth

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Blueprint, redirect, request, jsonify
 from services.auth_service import build_authorization_url, exchange_code_for_token
 
 auth_bp = Blueprint("auth", __name__)
+frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 
 @auth_bp.route("/auth/login", methods=["GET"])
@@ -19,10 +22,11 @@ def callback():
         return jsonify({"error": "Missing authorization code"}), 400
 
     try:
-        tokens = exchange_code_for_token(code)
-        return jsonify({
-            "message": "Authentication successful",
-            "tokens": tokens
-        })
+        _ = exchange_code_for_token(code)
+        # return jsonify({
+        #     "message": "Authentication successful",
+        #     "tokens": tokens
+        # })
+        return redirect(f"{frontend_url}/zf?auth=success")
     except Exception as e:
         return jsonify({"error": str(e)}), 500
